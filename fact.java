00a3bb31628cabe97c9c37d1daf5484930da1c0c
divya-kandhasamy <divya1314123@gmail.com>
2016-09-02 12:07:49 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -7,8 +7,13 @@ int f=1;
 System.out.println("Enter the number");
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
+if(n==0)
+System.out.println("Factorial is 1")
+else
+{
 for(int i=1;i<n;i++)
 f=f*i;
 System.out.println("Factorial is"+f);
+}
 }
 }
